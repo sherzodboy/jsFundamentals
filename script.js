6017daf1f047e3532cc64c317d83fc8d483548a0
sherzodboy <kholmamatovsh09@gmail.com>
2024-02-08 15:16:42 +0500

    js

---
+++ script.js
@@ -0,0 +1,263 @@
+// alert("hello js");
+// let js = "javascript is amazing";
+// console.log(js);
+// console.log("bir ikki uch");
+// console.log(12 + 8 + 10 - 5 - 23 + 43);
+
+// let person = "Tom";
+// let age = 23;
+// console.log(person);
+// console.log(age);
+
+// let firstName = "Tomes";
+// console.log(firstName);
+
+// let personAge = 23;
+
+// let firstNamePerson = "Bob";
+// console.log(firstNamePerson);
+
+// let years = 4;
+// console.log(years);
+
+// let jonesMatilda = "JM";
+// console.log(jonesMatilda);
+
+// let firstNamePerson = "Ann";
+// let personAge = "23";
+
+// let $function = "hi";
+// console.log(_function);
+
+// let _new = "yangi";
+// console.log($new);
+
+// let PI = 3.14;
+
+// let firstName = "Jone";
+// console.log(firstName);
+
+// let myFirstJob = "Programmer";
+// let myCurrentJob = "Teacher";
+
+/* let job1 = "Programmer";
+ let job2 = "Teacher";
+
+let personAge = 25;
+let firstName = "Jonas"; */
+
+// let personAge = 25;
+// let firstName = "Jonas";
+// console.log(typeof personAge);
+// console.log(typeof firstName);
+
+// let javascriptIsFun = "yes ";
+
+// console.log(typeof javascriptIsFun);
+
+// let children;
+// console.log(children);
+// console.log(typeof children);
+
+// let firstName = "Ann";
+// console.log(firstName);
+
+// let year;
+// console.log(year);
+// year = 2024;
+
+// prev lesson
+// let $function = "Hello";
+// let _new = "Javascript";
+// console.log($function);
+// console.log(_new);
+
+// console.log(typeof 123);
+// console.log(typeof "123");
+
+// let javascriptIsFun;
+// console.log(typeof javascriptIsFun);
+// javascriptIsFun = "yes";
+
+// let age = 30;
+// age = 31;
+// console.log(age);
+
+// const birthYear = 1998;
+// birthYear = 2000;
+
+// let job = "Programmer";
+
+// job2 = "coder"; // it is terrible error
+// console.log(job2);
+
+// const now = 2024;
+
+// const ageTom = now - 1990;
+// const ageAnn = now - 2000;
+
+// // console.log(ageTom, ageAnn);
+// console.log(2 ** 3); // 2 * 2 * 2 = 8
+
+// const firstName = "Leonel";
+// const lastName = "Messi";
+
+// console.log(firstName + " " + lastName);
+
+// let x = 10 + 5;
+// x += 10; // x = x + 10 = 25
+// x += 20; // x = 25 + 20 = 45
+// x -= 10; // x = 45 - 10
+// x++; // x = 35 + 1
+// x++;
+// x--;
+// x--;
+// console.log(x);
+
+// const now = 2024;
+
+// const ageTom = now - 1991; // 33
+// const ageAnn = now - 1990; // 34
+// // console.log(ageTom, ageAnn);
+
+// console.log(ageTom <= ageAnn);
+
+// const now = 2024;
+
+// console.log(now - 1990 > now - 1995); // 34 > 29
+
+// let x, y;
+
+// x = y = 25 - 10 - 5;
+// console.log(x, y);
+
+// let x, y, z;
+// x = y = z = (5 + 10 + 15 - 4 * 2) / 3;
+// console.log(x, y, z);
+
+// issue 1
+// const massMark = 78;
+// const heightMark = 1.69;
+
+// const massJohn = 92;
+// const heighJohn = 1.95;
+
+// const BMIMakr = massMark / heightMark ** 2;
+// const BMIJohn = massJohn / (heighJohn * heighJohn);
+
+// const markHigherBMI = BMIMakr > BMIJohn;
+
+// console.log(BMIMakr, BMIJohn, markHigherBMI);
+
+// const firstName = "Alibek";
+// const job = "oquvchi";
+// const birthYear = 2009;
+// const currentYear = 2024;
+//  mening ismim Alibek, 15 yoshli oquvchi
+
+// console.log("Mening ismim" + " " + firstName + ", " + " " + (currentYear - birthYear) + " " + "yoshli" + " " + job + "man");
+
+// console.log(
+//   `Mening ismim ${firstName}, ${currentYear - birthYear} yoshli ${job}`
+// );
+
+// console.log("Hello \n my name is \n Tom");
+// console.log(`Hello
+//  my name is
+//  Tom`);
+
+// if () {
+
+// } else {
+
+// }
+
+// const age = 18;
+
+// if (age > 18) {
+//   console.log("Siz prava olish yoshidasiz");
+// } else if (age < 18) {
+//   console.log("Siz prava yoshida emassiz");
+// } else {
+//   console.log("Error");
+// }
+
+// let birthYear = 1990;
+// let century = 20;
+// if (birthYear <= 2000) {
+//   console.log(century);
+// } else {
+//   console.log((century = 21));
+// }
+
+// const massMark = 78;
+// const heightMark = 1.69;
+
+// const massJohn = 92;
+// const heighJohn = 1.95;
+
+// const BMIMakr = massMark / heightMark ** 2;
+// const BMIJohn = massJohn / (heighJohn * heighJohn);
+
+// const markHigherBMI = BMIMakr > BMIJohn;
+
+// console.log(BMIMakr, BMIJohn);
+
+// if (BMIMakr > BMIJohn) {
+//   console.log(
+//     `Marrkni BMI ${BMIMakr} massasi Johni BMI ${BMIJohn} massasidan kattaroq`
+//   );
+// } else {
+//   console.log("false");
+// }
+
+// const inputYear = "1990";
+// console.log(Number(inputYear) + 20);
+
+// const firstName = "Tom";
+// console.log(Number(firstName));
+
+// console.log(String(123));
+// console.log(123);
+// console.log(Number("123") + 123);
+// console.log("I am " + 23 + " years old"); //
+
+// console.log("30" - "10" - 5);
+
+// console.log("20" * "2"); // return Number
+// console.log("20" / "2"); // also works
+
+// let n = "1" + 1;
+// n = n - 1;
+// console.log(n); // 10
+
+// console.log(2 + 3 + 4 + "5"); // 95
+// console.log("10" - "4" - "3" - 2 + "5"); // 15
+
+// in js 5ta falsy valueslar bor 0, “”, undefined, null, NaN
+
+// console.log(Boolean(0));
+// console.log(Boolean(""));
+// console.log(Boolean(undefined));
+// console.log(Boolean(null));
+// console.log(Boolean(NaN));
+
+// const money = 1;
+// if (money) {
+//   console.log("hammasini sarflama");
+// } else {
+//   console.log("sen darhol ish topishing kerak");
+// }
+
+// let height = 1.99;
+// if (height) {
+//   console.log("Hey height is defined");
+// } else {
+//   console.log("Ohh sorry height is undefined");
+// }
+
+// console.log(Boolean(1));
+// console.log(Boolean("hi"));
+// console.log(Boolean(true));
+// console.log(Boolean({}));
+// console.log(Boolean([]));
